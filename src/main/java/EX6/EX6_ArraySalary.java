1fd8064aa5bb2ddd5a710d4e489f718663417e9e
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-11-22 10:52:52 +0900

    問題１　給料表から、各社員の給料の全体に対する割合を求め、画面に表示する一次元配列のプログラムEX6_AttaySalary.javaを作成しました。

---
+++ src/main/java/EX6/EX6_ArraySalary.java
@@ -0,0 +1,25 @@
+public class EX6_ArraySalary {
+    public static void main(String[] args) {
+        /* 宣言と初期化 */
+        String[] arrayName = new String[] {"小暮次郎","川村麻衣","北上義徳",
+                                           "佐野洋子","松原雅彦"};
+        int[] arrayMoney = new int[] {184000,175000,261000,163000,226000};
+        double[] arrayRatio = new double[arrayMoney.length];
+        int Total = 0;
+
+        /* 計算 */
+        for (int i = 0; i < arrayMoney.length; i++) {
+            Total += arrayMoney[i];
+        }
+        for (int i = 0; i < arrayRatio.length; i++) {
+            arrayRatio[i] = (double)arrayMoney[i] / Total * 100;
+        }
+
+        /* 表示 */
+        System.out.println("★　給料の状況　★");
+        for (int i = 0; i < arrayName.length; i++) {
+            System.out.println(arrayName[i] + "\t" + arrayMoney[i] + "\t" + "約" + Math.round(arrayRatio[i]) + "%");
+        }
+        System.out.println("給料合計" + "\t" + Total);
+    }    
+}
